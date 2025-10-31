215b780c3c8a9874d1d7287e04c1def8ec8d772c
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-31 20:02:15 +0300

    feat(publisher) : created publisher

--- Rabbitmq_example/Program.cs
+++ Rabbitmq_example/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Created connection
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://xcqfhxlm:PpulfA_CKKshVziwYWdxB-4-C02Ue2XP@codfish.rmq.cloudamqp.com/xcqfhxlm");
+
+
+// Objects declared with using are automatically disposed when they go out of scope
+using IConnection connection = factory.CreateConnection();
+connection.CreateModel();
+using IModel channel = connection.CreateModel();
+
+
+//create queue
+
+channel.QueueDeclare(queue: "example_queue",exclusive:false,autoDelete:false);
+
+//send a message to queue
+
+byte[] message =  Encoding.UTF8.GetBytes("hii");
+channel.BasicPublish(exchange:"",routingKey :"example_queue",body:message);
+
+
+Console.Read();
