a68e0c3b0b5ed4fdce37bf7856145b67cca46c72
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-31 20:28:30 +0300

    feat(consumer) : created for basic consumer examples

---
+++ RabbitMQ_Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Diagnostics;
+using System.Text;
+
+
+// create connection 
+
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://xcqfhxlm:PpulfA_CKKshVziwYWdxB-4-C02Ue2XP@codfish.rmq.cloudamqp.com/xcqfhxlm");
+
+//create canal active connection
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//create queue
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+
+//read message from queue 
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue:"example_queue",false,consumer);
+consumer.Received += (sender, e) =>
+{
+    //received the message
+    //e.Body : kuyuktaki mesajin verisini getirecektir
+    //e.Body.Span or e.Body.ToArray(): kuyruktakı mesajın byt verisini getirecektir
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
+
