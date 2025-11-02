051fbaa3cffc0931d07f127b42f05e4cd3de93ca
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-02 20:26:03 +0300

    feat() created

---
+++ InMemory_Caching/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InMemory_Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("set/{name}")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name", "gncy");
+        }
+        [HttpGet]
+        public string Get2()
+        {
+            return _memoryCache.Get<string>("name");
+        }
+
+
+    }
+}
