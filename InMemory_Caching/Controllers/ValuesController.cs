2f72296a675fc9f61906c90e6a4e5be129629652
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-03 21:07:08 +0300

    feat(controller) : memory cache implemented

--- InMemory_Caching/Controllers/ValuesController.cs
+++ InMemory_Caching/Controllers/ValuesController.cs
@@ -18,17 +18,49 @@ namespace InMemory_Caching.Controllers
         {
             _memoryCache = memoryCache;
         }
-
+        /*
         [HttpGet("set/{name}")]
-        public void Set(string name)
+        public void SetName(string name)
         {
-            _memoryCache.Set("name", "gncy");
+            _memoryCache.Set("name", name);
         }
         [HttpGet]
-        public string Get2()
+        public string GetName()
         {
-            return _memoryCache.Get<string>("name");
+            if (_memoryCache.TryGetValue<string>("name",out string name))
+            {
+                return name.Substring(3);
+
+            }
+            return " ";
+            //var name  = _memoryCache.Get<string>("name");
+            //return name.Substring(3);
         }
+
+        */
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now,options : new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+
+         
+
+
+
+
+
+
+
 
 
     }
